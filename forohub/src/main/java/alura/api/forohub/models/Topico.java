0e71f542c307c7143dcd0d526d37fe817bf396e2
plzkllm <axl_parks@yahoo.com.pe>
2025-01-21 17:58:18 -0500

    Creacion de la clase base topico y diseñando el controlador de la API REST

---
+++ forohub/src/main/java/alura/api/forohub/models/Topico.java
@@ -0,0 +1,69 @@
+package alura.api.forohub.models;
+
+import java.util.Date;
+
+public class Topico {
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private Date fechaDeCreacion;
+    private String status;
+    private String autor;
+    private String curso;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Date getFechaDeCreacion() {
+        return fechaDeCreacion;
+    }
+
+    public void setFechaDeCreacion(Date fechaDeCreacion) {
+        this.fechaDeCreacion = fechaDeCreacion;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+}
